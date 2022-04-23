cad39cf3958a45d97ec14080cc9c8e59d490cf97
Jiayi Weng <trinkle23897@gmail.com>
2022-04-23 17:09:33 -0400

    init_qpos and init_qvel

--- ant.cc
+++ ant.cc
@@ -7,5 +7,21 @@ int main(int argc, char const *argv[])
     char error[1000];
     mjModel* m = mj_loadXML(argv[1], 0, error, 1000);
     assert(m);
+    mjData *d = mj_makeData(m);
+    assert(d);
+    mjtNum *init_qpos = new mjtNum[m->nq];
+    memcpy(init_qpos, d->qpos, sizeof(mjtNum) * m->nq);
+    mjtNum *init_qvel = new mjtNum[m->nv];
+    memcpy(init_qvel, d->qvel, sizeof(mjtNum) * m->nv);
+    printf("nq: %d | ", m->nq);
+    for (int i = 0; i < m->nq; ++i) {
+        printf("%.3f ", init_qpos[i]);
+    }
+    puts("");
+    printf("nv: %d | ", m->nv);
+    for (int i = 0; i < m->nv; ++i) {
+        printf("%.3f ", init_qvel[i]);
+    }
+    puts("");
     return 0;
 }
