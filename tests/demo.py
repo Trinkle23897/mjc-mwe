2365bb69f59b3789078a5e29f451e9e9f282a328
Jiayi Weng <trinkle23897@gmail.com>
2022-04-26 08:21:30 -0400

    update demo

--- tests/demo.py
+++ tests/demo.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from mjc_mwe import (
     AntEnv,
@@ -37,15 +38,18 @@ if __name__ == '__main__':
         "humanoidstandup.xml",
         "inverted_double_pendulum.xml",
         "inverted_pendulum.xml",
-        "point.xml",
         "pusher.xml",
         "reacher.xml",
         "swimmer.xml",
         "walker2d.xml",
     ]
     for env_cls, xml in zip(env_clses, xml_files):
+        if sys.argv[-1] not in xml:
+            continue
         print(env_cls)
         env = env_cls(xml_file=os.path.abspath(f"../mjc_mwe/assets/{xml}"))
         print(env)
+        print(env.observation_space)
+        print(env.action_space)
         print(env.reset())
         print(env.step(env.action_space.sample()))
